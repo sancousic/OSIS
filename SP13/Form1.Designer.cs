3f91edebf5912b5453e1c130122645e497be0df2
Alexandr <znosok2012@gmail.com>
2020-04-18 16:51:00 +0300

    add osis 13

--- SP13/Form1.Designer.cs
+++ SP13/Form1.Designer.cs
@@ -29,12 +29,112 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.belowNormalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboveNormalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.highToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.idleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.normalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.realTimeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(356, 433);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(374, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(414, 433);
+            this.listBox2.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.belowNormalToolStripMenuItem,
+            this.aboveNormalToolStripMenuItem,
+            this.highToolStripMenuItem,
+            this.idleToolStripMenuItem,
+            this.normalToolStripMenuItem,
+            this.realTimeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 158);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // belowNormalToolStripMenuItem
+            // 
+            this.belowNormalToolStripMenuItem.Name = "belowNormalToolStripMenuItem";
+            this.belowNormalToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.belowNormalToolStripMenuItem.Text = "BelowNormal";
+            this.belowNormalToolStripMenuItem.Click += new System.EventHandler(this.belowNormalToolStripMenuItem_Click);
+            // 
+            // aboveNormalToolStripMenuItem
+            // 
+            this.aboveNormalToolStripMenuItem.Name = "aboveNormalToolStripMenuItem";
+            this.aboveNormalToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.aboveNormalToolStripMenuItem.Text = "AboveNormal";
+            // 
+            // highToolStripMenuItem
+            // 
+            this.highToolStripMenuItem.Name = "highToolStripMenuItem";
+            this.highToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.highToolStripMenuItem.Text = "High";
+            // 
+            // idleToolStripMenuItem
+            // 
+            this.idleToolStripMenuItem.Name = "idleToolStripMenuItem";
+            this.idleToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.idleToolStripMenuItem.Text = "Idle";
+            // 
+            // normalToolStripMenuItem
+            // 
+            this.normalToolStripMenuItem.Name = "normalToolStripMenuItem";
+            this.normalToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.normalToolStripMenuItem.Text = "Normal";
+            // 
+            // realTimeToolStripMenuItem
+            // 
+            this.realTimeToolStripMenuItem.Name = "realTimeToolStripMenuItem";
+            this.realTimeToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.realTimeToolStripMenuItem.Text = "RealTime";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem belowNormalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboveNormalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem highToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem idleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem normalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem realTimeToolStripMenuItem;
     }
 }
 
--- SP13/Form1.cs
+++ SP13/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,95 @@ namespace SP13
         public Form1()
         {
             InitializeComponent();
+            var processes = Process.GetProcesses();
+            foreach(var proc in processes)
+            {
+                listBox1.Items.Add(new ProcessItem(proc));                
+            }
+            contextMenuStrip1.ItemClicked += ContextMenuStrip1_ItemClicked;
+            listBox1.ContextMenuStrip = contextMenuStrip1;
+            listBox1.MouseUp += ListBox1_MouseUp;
+        }
+
+        private void ListBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Right)
+            {
+                listBox1.SelectedIndex = listBox1.IndexFromPoint(e.X, e.Y);
+            }
+        }
+
+        private void ContextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            try
+            {
+                var i = listBox1.SelectedIndex;
+                var temp = (listBox1.SelectedItem as ProcessItem).Process;
+                Enum.TryParse<ProcessPriorityClass>(e.ClickedItem.Text, out ProcessPriorityClass priority);
+                temp.PriorityClass = priority;
+                listBox1.Items.Remove(listBox1.SelectedItem);
+                listBox1.Items.Insert(i, new ProcessItem(temp));
+                listBox1.SelectedIndex = i;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+                
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                listBox2.Items.Clear();
+                var process = (listBox1.SelectedItem as ProcessItem).Process;
+                try
+                {
+                    foreach (var module in process.Modules)
+                    {
+                        listBox2.Items.Add(module.ToString());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    listBox2.Items.Add(ex.Message);
+                }
+            }
+        }
+
+        private void belowNormalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+    class ProcessItem
+    {
+        public ProcessItem(Process _process)
+        {
+            Process = _process;
+        }
+        public Process Process { get; set; }
+        public override string ToString()
+        {
+            try
+            {
+                return $"{Process.ProcessName} Priority:{Process.PriorityClass.ToString()}";
+            }
+            catch
+            {
+                return $"{Process.ProcessName}";
+            }     
         }
     }
 }
