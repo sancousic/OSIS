2e9a56e028da6f4cd0aa8434502ef845530aaf37
Alexandr <znosok2012@gmail.com>
2020-04-18 18:48:26 +0300

    added 14.3.1

---
+++ SP14/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace OSIS14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 415);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(587, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(605, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(731, 419);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(63, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Regular";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name,
+            this.Type,
+            this.Value});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(780, 397);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "Имя";
+            this.Name.Name = "Name";
+            this.Name.Width = 260;
+            // 
+            // Type
+            // 
+            this.Type.HeaderText = "Тип";
+            this.Type.Name = "Type";
+            this.Type.Width = 260;
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Значение";
+            this.Value.Name = "Value";
+            this.Value.Width = 260;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+    }
+}
+
---
+++ SP14/Form1.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OSIS14
+{
+    // 14.3.1 Поиск в реесте значений, в том числе по шаблону
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            RegistryKey[] registryKeys = { Registry.ClassesRoot, Registry.CurrentConfig, Registry.CurrentUser,
+                Registry.LocalMachine, Registry.PerformanceData, Registry.Users };
+            if(checkBox1.Checked)
+            {
+                Regex regex = new Regex(textBox1.Text);
+                foreach (var item in registryKeys)
+                {
+                    await Task.Run(() => search(item, regex));
+                }
+            }
+            else
+            {
+                foreach (var item in registryKeys)
+                {
+                    await Task.Run(() => search(item, textBox1.Text));
+                }
+            }
+        }
+        async void search(RegistryKey key, Regex val)
+        {
+            try
+            {
+                var subs = key.GetSubKeyNames();
+                if (subs.Length > 0)
+                {
+                    foreach (var subkeyName in subs)
+                    {
+                        var subkey = key.OpenSubKey(subkeyName);
+                        if(subkey != null)
+                            await Task.Run(() => search(subkey, val));
+                    }
+                }
+                var values = key.GetValueNames();
+                foreach (var v in values)
+                {
+                    var value = key.GetValue(v).ToString();
+                    if (val.IsMatch(value))
+                        await Task.Run(() =>
+                        {
+                            dataGridView1.Invoke(new Action(() =>
+                            {
+                                dataGridView1.Rows.Add(string.IsNullOrEmpty(v) ? "Default name" : v,
+                                    key.GetValueKind(v), value);
+                            }));
+                            Thread.Sleep(10);
+                        });
+                }
+               
+            }
+            catch
+            {
+            }
+        }
+        async void search(RegistryKey key, string val)
+        {
+            try
+            {
+                var subs = key.GetSubKeyNames();
+                if (subs.Length > 0)
+                {
+                    foreach (var subkeyName in subs)
+                    {
+                        var subkey = key.OpenSubKey(subkeyName);
+                        if (subkey != null)
+                            await Task.Run(() => search(subkey, val));
+                    }
+                }
+                var values = key.GetValueNames();
+                foreach (var v in values)
+                {
+                    var value = key.GetValue(v).ToString();
+                    if (val == value)
+                        await Task.Run(() =>
+                        {
+                            dataGridView1.Invoke(new Action(() =>
+                            {
+                                dataGridView1.Rows.Add(string.IsNullOrEmpty(v) ? "Default name" : v,
+                                    key.GetValueKind(v), value);
+                            }));
+                            Thread.Sleep(10);
+                        });
+                }
+
+            }
+            catch
+            {
+            }
+        }
+    }
+}
